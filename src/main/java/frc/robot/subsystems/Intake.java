eb5cd80fec8e35347f8944154cf1328a2774b32d
FRCTeam1506 <56982029+FRCTeam1506@users.noreply.github.com>
2026-01-31 18:51:24 -0500

    01/31/26
    First Commit. Intake works!!

---
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,67 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Intake extends SubsystemBase {
+  private TalonFX intake = new TalonFX(60);
+  private TalonFX intakeLift = new TalonFX(14);
+
+
+  /** Creates a new Intake. */
+  public Intake() {
+    var talonFXConfigs = new TalonFXConfiguration();
+
+    // talonFXConfigs.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+
+    TalonFXConfiguration config = new TalonFXConfiguration();
+    config.CurrentLimits.StatorCurrentLimitEnable = true;
+    config.CurrentLimits.StatorCurrentLimit = 80;
+
+    config.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+
+    intakeLift.getConfigurator().apply(config);
+    intake.getConfigurator().apply(config);
+
+    var motionMagicConfigs = talonFXConfigs.MotionMagic;
+    motionMagicConfigs.MotionMagicCruiseVelocity = 220; // 80 rps cruise velocity //60 rps gets to L4 in 1.92s //100 //160 //220 before 3/20 bc elevator maltensioned //220 FRCC
+    motionMagicConfigs.MotionMagicAcceleration = 260; // 160 rps/s acceleration (0.5 seconds) //220
+    motionMagicConfigs.MotionMagicJerk = 3200; // 1600 rps/s^2 jerk (0.1 seconds)
+
+    // set slot 0 gains
+    var slot0Configs = talonFXConfigs.Slot0;
+    slot0Configs.kS = 0.24; // add 0.24 V to overcome friction
+    slot0Configs.kV = 0.12; // apply 12 V for a target velocity of 100 rps
+    // PID runs on position
+    slot0Configs.kP = 2.5; //4.8
+    slot0Configs.kI = 0;
+    slot0Configs.kD = 0.1;
+
+    config.Slot0 = slot0Configs;
+
+
+    intakeLift.getConfigurator().apply(motionMagicConfigs);
+    intake.getConfigurator().apply(slot0Configs); 
+
+  }
+
+  public void runIntake(Double speed) {
+    intake.set(speed);
+  } 
+  public void intakeLift(Double Pos) {
+    intakeLift.setPosition(Pos);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
+
